1f6fc44033a9c797c658908db4a0c4f1e5e74dfe
Ruslan <@>
2024-01-04 11:25:37 +0300

    feat: add unit tests for validators

---
+++ backend/tests/To-Do-List.Application.UnitTests/Validators/TodoItemDTOValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using To_Do_List.Application.Common.Validators;
+using To_Do_List.Application.DTOs;
+
+namespace To_Do_List.Application.UnitTests.Validators;
+
+public class TodoItemDTOValidatorTests
+{
+    private readonly TodoItemDTOValidator _validator;
+
+    public TodoItemDTOValidatorTests()
+    {
+        _validator = new TodoItemDTOValidator();
+    }
+
+    [Fact] 
+    public void Should_Have_Error_When_Title_Is_Empty()
+    {
+        // arrange
+        var todoItemDto = new TodoItemDTO() { Title = string.Empty };
+        
+        // act
+        var result = _validator.Validate(todoItemDto);
+        
+        // assert
+        Assert.False(result.IsValid);
+    }
+}
