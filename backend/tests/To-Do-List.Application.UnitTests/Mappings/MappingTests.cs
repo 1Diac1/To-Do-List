f2fe4bd2b18c184451bec6f74cf45ee1224ad2f5
Ruslan <@>
2024-01-04 08:27:50 +0300

    feat: add unit tests for mappings

---
+++ backend/tests/To-Do-List.Application.UnitTests/Mappings/MappingTests.cs
@@ -0,0 +1,28 @@
+﻿using To_Do_List.Application.Mappings;
+using To_Do_List.Application.DTOs;
+using To_Do_List.Domain.Entities;
+using System.Drawing;
+using AutoMapper;
+
+namespace To_Do_List.Application.UnitTests.Mappings;
+
+public class MappingTests
+{
+    [Fact]
+    public void Should_Map_Entity_To_Dto_Correctly()
+    {
+        // arrange
+        var todoTag = new TodoTag() { Color = KnownColor.Aqua }; 
+        var todoItem = new TodoItem { Tags = new List<TodoTag>() { todoTag } };
+
+        var mappingProfiles = new List<Profile>() { new TodoItemMapping(), new TodoTagMapping() };
+        var mapper = new Mapper(new MapperConfiguration(cfg => 
+            cfg.AddProfiles(mappingProfiles)));
+        
+        // act
+        var dto = mapper.Map<TodoItemDTO>(todoItem);
+
+        // assert
+        Assert.Equal(todoTag.Color, dto.Tags.FirstOrDefault()!.Color);
+    }
+}
